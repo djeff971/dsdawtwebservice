1f664309f4bf69c9b7787bb6c51862f33acd51fe
djeff971 <kyuside@gmail.com>
2015-01-13 16:04:54 +0100

    ok

--- src/pkg/webservice/first/RESTRessource.java
+++ src/pkg/webservice/first/RESTRessource.java
@@ -7,45 +7,47 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.log4j.Logger;
-
 import pkg.webservice.beans.Tweet;
 import pkg.webservice.beans.User;
 import pkg.webservice.dao.DbHelper;
 
 @Path("/res")
 public class RESTRessource {
-/*	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String testText() {
-		return "just a text test";
-	}
+	/*
+	 * @GET
+	 * 
+	 * @Produces(MediaType.TEXT_PLAIN) public String testText() { return
+	 * "just a text test"; }
+	 * 
+	 * @GET
+	 * 
+	 * @Produces(MediaType.TEXT_XML) public String testXML() { return
+	 * "<body><i>just</i> an XML <b>test</b></body>"; }
+	 */
 
-	@GET
-	@Produces(MediaType.TEXT_XML)
-	public String testXML() {
-		return "<body><i>just</i> an XML <b>test</b></body>";
-	}*/
-	
-	
-	
-	DbHelper dbhelp;
-	//static Logger log = Logger.getLogger(RESTRessource.class);
+	// DbHelper dbhelp;
+	// static Logger log = Logger.getLogger(RESTRessource.class);
 
-
-	public void updateData() {
-		//TODO: checkout if the TB is empty
-		dbhelp.updateData();
-	}
+	/*
+	 * public void updateData() { //TODO: checkout if the TB is empty
+	 * dbhelp.updateData(); }
+	 */
 
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<User> getUsers() {
-//		log.info("let's go into db...");
-		return dbhelp.getUsers();
-	}
+		// log.info("let's go into db...");
+		DbHelper dbhelper = new DbHelper();
+		List<User> users = dbhelper.getUsers();
 
-	public List<Tweet> getTweets(String nickname) {
-		return dbhelp.getTweets(nickname);
+		for (User u : users) {
+			System.out.println(u.getId() + u.getName());
+		}
+		return dbhelper.getUsers();
+		// return dbhelp.getUsers();
 	}
+	/*
+	 * public List<Tweet> getTweets(String nickname) { return
+	 * dbhelp.getTweets(nickname); }
+	 */
 }
